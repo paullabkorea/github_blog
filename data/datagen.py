2de1a4710f4634f80bdf97693a959a7c94ef122d
paullabkorea <paullabkorea@gmail.com>
2024-02-11 02:12:43 +0900

    datagen.py 파일 통합

--- data/datagen.py
+++ data/datagen.py
@@ -1,21 +1,54 @@
 # 해당 파일은 테스트용도이며 실제 서비스에는 포함되지 않습니다.
-# blog폴더에 파일 목록을 읽어 local_blogList.json 파일을 생성해주는 파일입니다.
-# 데이터 형식
+# blog 폴더에 [20240101]_[title0101]_[test]_[]_[테스트용입니다]_[].md 파일부터 [20241230]_[title1230]_[test]_[]_[테스트용입니다]_[].md까지 원하는 월을 선택해 생성해주는 파일입니다.
+# 동시에 data 폴더에 local_blogList.json 파일을 생성해주는 파일입니다.
+# 원하는 월은 python 파일에서 input 함수로 입력받습니다.
+
 """
-[
-    {
-        "name": "[20240118]_[title_test]_[python]_[]_[파이썬은 놀라운 언어입니다.]_[].md",
-        "download_url": "/blog/[20240118]_[title_test]_[python]_[]_[파이썬은 놀라운 언어입니다.]_[].md"
-    },
-    {
-        "name": "[20240119]_[title_test2]_[django]_[test.png]_[파이썬은 정말 재미있습니다.]_[].md",
-        "download_url": "/blog/[20240119]_[title_test2]_[django]_[test.png]_[파이썬은 정말 재미있습니다.]_[].md"
-    }
-]
+# 테스트용입니다.
+
+---
+
+* `20240101_[title0101]_[test]_[]_[테스트용입니다]_[]`.md 파일입니다.
+```python
+def hello():
+    print('하이라이팅 테스트입니다.')
+```
 """
 
 import os
 import json
+import datetime
+
+# 날짜 생성
+start_date = datetime.date(2024, 1, 1)
+end_date = datetime.date(2024, 12, 30)
+delta = datetime.timedelta(days=1)
+date_list = []
+input_month = int(input("몇 월을 생성하시겠습니까? "))
+
+while start_date <= end_date:
+    date_list.append(start_date)
+    start_date += delta
+
+# 파일 생성
+for date in date_list:
+    if date.month == input_month:
+        # f-string 사용
+        file_name = f'[{date.strftime("%Y%m%d")}]_[title{date.strftime("%m%d")}]_[test]_[]_[테스트용입니다]_[].md'
+        file_text = f"""
+# 테스트용입니다.
+
+---
+
+* `{date.strftime("%Y%m%d")}_{file_name}` 파일입니다.
+```python
+def hello():
+    print('하이라이팅 테스트입니다.')
+```
+"""
+        # 경로를 ../blog로 설정하여 파일 생성
+        with open(f"../blog/{file_name}", "w", encoding="utf-8") as f:
+            f.write(file_text)
 
 # 파일 목록 읽기
 blog_list = os.listdir("../blog")
