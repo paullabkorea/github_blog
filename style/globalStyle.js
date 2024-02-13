c6bd26fc3f6b99fcfda444648f2490484820ec32
baekg <baekg.dev@gmail.com>
2024-02-13 15:07:18 +0900

    :bneto: Add: 프로필 이미지 추가

--- style/globalStyle.js
+++ style/globalStyle.js
@@ -73,6 +73,6 @@ const bloglistCardDateStyle = `text-graylv3 text-sm inline-block font-normal`;
 const searchInputStyle = `absolute top-20 right-8 w-[220px] h-10 rounded-md border border-gray-300 pl-2 text-base font-bold text-gray-600 outline-none box-border transition duration-300 ease-in-out shadow-none bg-white bg-clip-padding`;
 
 // category 스타일
-const categoryContainerStyle = `hidden md:flex flex-col md:w-[220px] fixed top-24 right-8 overflow-y-auto border p-4 rounded-md bg-white shadow-sm z-10`;
-const categoryItemStyle = `text-base font-bold text-gray-600 mb-2 cursor-pointer`;
-const categoryItemCountStyle = `text-sm font-normal text-gray-400 ml-2`;
+const categoryContainerStyle = `hidden flex-col md:w-[220px] overflow-y-auto p-4 rounded-md bg-white shadow-sm z-10`;
+const categoryItemStyle = `text-base font-birnak text-gray-600 mb-2 cursor-pointer`;
+const categoryItemCountStyle = `text-base font-normal text-graylv2 ml-1`;
